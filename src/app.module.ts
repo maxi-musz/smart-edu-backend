ec146fe572b997a6084325b9a50782751958352e
Maximus <bernardmayowaa@gmail.com>
2025-05-31 12:55:59 +0100

    completed folder structure setuop

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HelloModule } from './hello/hello.module';
+import { AdminModule } from './admin/admin.module';
+import { SchoolModule } from './school/school.module';
 
 @Module({
-  imports: [HelloModule],
+  imports: [HelloModule, AdminModule, SchoolModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/school/director/director.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { DashboardModule } from './dashboard/dashboard.module';
+import { TeachersModule } from './teachers/teachers.module';
+import { StudentsModule } from './students/students.module';
+import { FinanceModule } from './finance/finance.module';
+import { CoursesModule } from './courses/courses.module';
+import { SchedulesModule } from './schedules/schedules.module';
+import { NotificationsModule } from './notifications/notifications.module';
+import { SettingsModule } from './settings/settings.module';
+import { ProfilesModule } from './profiles/profiles.module';
+
+@Module({
+  imports: [DashboardModule, TeachersModule, StudentsModule, FinanceModule, CoursesModule, SchedulesModule, NotificationsModule, SettingsModule, ProfilesModule]
+})
+export class DirectorModule {}
